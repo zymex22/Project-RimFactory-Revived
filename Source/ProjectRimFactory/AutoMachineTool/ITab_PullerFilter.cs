e85c88cde3f7ad39d35f4d1348c6e09d2d51628c
lilwhitemouse <lilmouse@littlewhitemouse.net>
2020-07-25 15:52:06 -0400

    Make filter say Filter instead of Storage

--- Source/ProjectRimFactory/AutoMachineTool/ITab_PullerFilter.cs
+++ Source/ProjectRimFactory/AutoMachineTool/ITab_PullerFilter.cs
@@ -11,63 +11,69 @@ using static ProjectRimFactory.AutoMachineTool.Ops;
 
 namespace ProjectRimFactory.AutoMachineTool
 {
-    //class ITab_PullerFilter : ITab_Storage
-    //{
+    // Have an ITab_Storage that says "Filter" instead of "Storage"
+    class ITab_PullerFilter : ITab_Storage {
+        public ITab_PullerFilter() : base() {
+            this.labelKey = "Filter";
+        }
+        // Everything else is vanilla, so any changes anyone makes to ITab_Storage
+        //   (such as RSA's search function!) *should* work just fine for us!
+#if false
+        private static readonly Vector2 WinSize = new Vector2(300f, 500f);
 
-    //    private static readonly Vector2 WinSize = new Vector2(300f, 500f);
+        public ITab_PullerFilter()
+        {
+            this.size = WinSize;
+            this.labelKey = "PRF.AutoMachineTool.Puller.OutputItemFilter.TabName";
 
-    //    public ITab_PullerFilter()
-    //    {
-    //        this.size = WinSize;
-    //        this.labelKey = "PRF.AutoMachineTool.Puller.OutputItemFilter.TabName";
+            this.description = "PRF.AutoMachineTool.Puller.OutputItemFilter.Description".Translate();
+        }
 
-    //        this.description = "PRF.AutoMachineTool.Puller.OutputItemFilter.Description".Translate();
-    //    }
+        private string description;
 
-    //    private string description;
+        private Building_ItemPuller Puller
+        {
+            get => (Building_ItemPuller)this.SelThing;
+        }
 
-    //    private Building_ItemPuller Puller
-    //    {
-    //        get => (Building_ItemPuller)this.SelThing;
-    //    }
+        public override void OnOpen()
+        {
+            base.OnOpen();
 
-    //    public override void OnOpen()
-    //    {
-    //        base.OnOpen();
+            this.groups = this.Puller.Map.haulDestinationManager.AllGroups.ToList();
+        }
 
-    //        this.groups = this.Puller.Map.haulDestinationManager.AllGroups.ToList();
-    //    }
+        private List<SlotGroup> groups;
 
-    //    private List<SlotGroup> groups;
+        public override bool IsVisible => Puller.Filter != null;
 
-    //    public override bool IsVisible => Puller.Filter != null;
+        private Vector2 scrollPosition;
 
-    //    private Vector2 scrollPosition;
+        protected override void FillTab()
+        {
+            Listing_Standard list = new Listing_Standard();
+            Rect inRect = new Rect(0f, 0f, WinSize.x, WinSize.y).ContractedBy(10f);
 
-    //    protected override void FillTab()
-    //    {
-    //        Listing_Standard list = new Listing_Standard();
-    //        Rect inRect = new Rect(0f, 0f, WinSize.x, WinSize.y).ContractedBy(10f);
+            list.Begin(inRect);
+            list.Gap();
 
-    //        list.Begin(inRect);
-    //        list.Gap();
+            var rect = list.GetRect(40f);
+            Widgets.Label(rect, this.description);
+            list.Gap();
 
-    //        var rect = list.GetRect(40f);
-    //        Widgets.Label(rect, this.description);
-    //        list.Gap();
+            rect = list.GetRect(30f);
+            if (Widgets.ButtonText(rect, "PRF.AutoMachineTool.Puller.FilterCopyFrom".Translate()))
+            {
+                Find.WindowStack.Add(new FloatMenu(groups.Select(g => new FloatMenuOption(g.parent.SlotYielderLabel(), () => this.Puller.Filter.CopyAllowancesFrom(g.Settings.filter))).ToList()));
+            }
+            list.Gap();
 
-    //        rect = list.GetRect(30f);
-    //        if (Widgets.ButtonText(rect, "PRF.AutoMachineTool.Puller.FilterCopyFrom".Translate()))
-    //        {
-    //            Find.WindowStack.Add(new FloatMenu(groups.Select(g => new FloatMenuOption(g.parent.SlotYielderLabel(), () => this.Puller.Filter.CopyAllowancesFrom(g.Settings.filter))).ToList()));
-    //        }
-    //        list.Gap();
+            list.End();
+            var height = list.CurHeight;
 
-    //        list.End();
-    //        var height = list.CurHeight;
+            ThingFilterUI.DoThingFilterConfigWindow(inRect.BottomPartPixels(inRect.height - height), ref this.scrollPosition, this.Puller.Filter);
 
-    //        ThingFilterUI.DoThingFilterConfigWindow(inRect.BottomPartPixels(inRect.height - height), ref this.scrollPosition, this.Puller.Filter);
-
-    //    }
-    //}
+        }
+#endif
+    }
 }
