6af2c0c2e20f71e3c04d9d4d935cc7b8f61e8a95
zyMex <41029070+zymex22@users.noreply.github.com>
2020-09-05 23:54:10 +0200

    initial port try AMT/SAL
    initial port try AMT/SAL

---
+++ Source/ProjectRimFactory/AutoMachineTool/PlaceWorker_AutoMachineTool.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RimWorld;
+using Verse;
+using Verse.AI;
+using UnityEngine;
+
+using static ProjectRimFactory.AutoMachineTool.Ops;
+
+namespace ProjectRimFactory.AutoMachineTool
+{
+    public class PlaceWorker_AutoMachineTool : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+        {
+            var r = base.AllowsPlacing(checkingDef, loc, rot, map, thingToIgnore);
+            if (r.Accepted)
+            {
+                if ((loc + rot.FacingCell).GetThingList(map)
+                    .Where(t => t.def.category == ThingCategory.Building)
+                    .SelectMany(t => Option(t as Building_WorkTable))
+                    .Where(b => b.InteractionCell == loc).Count() == 0)
+                {
+                    return new AcceptanceReport("NR_AutoMachineTool.PlaceNotAllowed".Translate());
+                }
+                return r;
+            }
+            else
+            {
+                return r;
+            }
+        }
+    }
+}
