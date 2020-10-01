e1f062dc9f007f7b0ed101e2d97627da60aff997
lilwhitemouse <lilmouse@littlewhitemouse.net>
2020-10-01 03:39:19 -0400

    More subtle arrow is easier on eyes

--- Source/ProjectRimFactory/AutoMachineTool/PlaceWorker_Arrow.cs
+++ Source/ProjectRimFactory/AutoMachineTool/PlaceWorker_Arrow.cs
@@ -13,12 +13,13 @@ namespace ProjectRimFactory.AutoMachineTool
 {
     public class PlaceWorker_Arrow : PlaceWorker
     {
+        public static readonly Material arrow = FadedMaterialPool.FadedVersionOf(MaterialPool.MatFrom(RS.Arrow), .6f);
         public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol, Thing thing = null)
         {
             base.DrawGhost(def, center, rot, ghostCol, thing);
             var pos = center.ToVector3Shifted();
             pos.y = AltitudeLayer.LightingOverlay.AltitudeFor();
-            Graphics.DrawMesh(MeshPool.plane10, pos, rot.AsQuat, MaterialPool.MatFrom(RS.Arrow), 0);
+            Graphics.DrawMesh(MeshPool.plane10, pos, rot.AsQuat, arrow, 0);
         }
     }
 }
