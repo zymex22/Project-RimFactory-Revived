d9f81e2e7d689a92126182c3aa014d56f532a68b
Sn1p3rr3c0n <sn1p3rr3c0n@gmail.com>
2020-10-10 12:03:07 +0200

    Added code to support refuling Mortars

--- Source/ProjectRimFactory/Industry/Building_FuelingMachine.cs
+++ Source/ProjectRimFactory/Industry/Building_FuelingMachine.cs
@@ -29,10 +29,18 @@ namespace ProjectRimFactory.Industry
                 //    than one thing, make the loop here and put some breaking logic
                 //    instead of all the "return;"s below)
                 CompRefuelable refuelableComp=null;
+                CompChangeableProjectile changeableProjectileComp = null;
+                
                 foreach (Thing tmpThing in Map.thingGrid.ThingsListAt(FuelableCell)) {
-                    if (tmpThing is Building) refuelableComp=(tmpThing as Building).GetComp<CompRefuelable>();
-                    if (refuelableComp != null) break;
+                    if (tmpThing is Building)
+                    {
+                        refuelableComp = (tmpThing as Building).GetComp<CompRefuelable>();
+                        //This if for Mortar like buildings
+                        changeableProjectileComp = (tmpThing as Building_TurretGun)?.gun?.TryGetComp<CompChangeableProjectile>();
+                    }
+                    if (refuelableComp != null || changeableProjectileComp != null) break;
                 }
+
                 if (refuelableComp != null) {
                     // Check if there is already enough fuel:
                     //  (because Fuel is a float, we check the current fuel + .99, so it
@@ -69,6 +77,35 @@ namespace ProjectRimFactory.Industry
                         }
                     }
                 }
+                else if (changeableProjectileComp != null)
+                {
+                    //Check if we need to load it
+                    if (changeableProjectileComp.Loaded == false)
+                    {
+                        foreach (IntVec3 cell in GenAdj.CellsAdjacent8Way(this))
+                        {
+                            List<Thing> l = Map.thingGrid.ThingsListAt(cell);
+                            for (int i = l.Count - 1; i >= 0; i--)
+                            { // count down because items may be destroyed
+                                Thing item = l[i];
+                                // Without this check, if there is something that is fueled by
+                                //     minified Power Conduits (weird, but ...possible?), then
+                                //     our FuelingMachine will happily rip conduits out of the
+                                //     ground to fuel it.  I'm okay with this behavior.
+                                //     Feature.  Not a bug.
+                                // But if it ever causes a problem, uncomment this check:
+                                // if (item.def.category != ThingCategory.Item) continue;
+                                if (changeableProjectileComp.allowedShellsSettings.filter.Allows(item))
+                                {
+                                    //Load Item
+                                    changeableProjectileComp.LoadShell(item.def, 1);
+                                    if (changeableProjectileComp.Loaded) return;
+                                }
+                            }
+                        }
+                    }
+                }
+            
             }
         }
         public override void DrawExtraSelectionOverlays()
