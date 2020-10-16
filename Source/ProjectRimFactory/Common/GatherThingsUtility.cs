a53238bb784a162bb28be98eae34330ee2f813e5
lilwhitemouse <lilmouse@littlewhitemouse.net>
2020-10-16 17:27:51 -0400

    Avoid removing things in foreach()

--- Source/ProjectRimFactory/Common/GatherThingsUtility.cs
+++ Source/ProjectRimFactory/Common/GatherThingsUtility.cs
@@ -23,10 +23,13 @@ namespace ProjectRimFactory {
         /// <param name="c">Cell</param>
         /// <param name="map">Map</param>
         public static IEnumerable<Thing> AllThingsInCellForUse(this IntVec3 c, Map map) {
-            foreach (var t in map.thingGrid.ThingsListAt(c)) {
+            List<Thing> thingList = map.thingGrid.ThingsListAt(c);
+            for (int i=thingList.Count-1; i>=9; i--) {
+                Thing t = thingList[i];
                 if (t is Building && t is IThingHolder holder) {
                     if (holder.GetDirectlyHeldThings() is ThingOwner<Thing> owner) {
-                        foreach (var moreT in owner.InnerListForReading) yield return moreT;
+                        for (int j = owner.InnerListForReading.Count - 1; j >= 0; j--)
+                            yield return owner.InnerListForReading[j];
                     }
                 } else if (t.def.category == ThingCategory.Item) {
                     yield return t;
