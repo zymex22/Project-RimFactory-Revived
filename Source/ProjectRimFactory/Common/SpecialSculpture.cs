e689934b3ee58ce20c81859b98f82348f6e94b9e
lilwhitemouse <lilmouse@littlewhitemouse.net>
2020-11-11 21:27:27 -0500

    Allow multiple copies of memorial artwork

--- Source/ProjectRimFactory/Common/SpecialSculpture.cs
+++ Source/ProjectRimFactory/Common/SpecialSculpture.cs
@@ -7,45 +7,80 @@ using HarmonyLib;
 namespace ProjectRimFactory {
     // Note: Other places that touch SpecialSculputre:
     //   ProjectRimFactor_ModComponent has the list of all of them
-    //   RS.cs initializes the resources (grahpics)
+    //   RS.cs initializes the resources (graphics)
     //   PRFGameComponent has the list of current special sculptures in a game
     //   HarmonyPatches/CompArt_GenerateImageDescription makes the description show up in game
+    // General outline in code:
+    //   1. RimWorld starts; ModComponenet initializes all available options
+    //      graphics inits
+    //   2. Game starts/loads
+    //      master list cleansed of any extra data
+    //      If loading game: GameComponent loads own list
+    //                       these update some values from master list
+    //                       at end of load, each turns it sculpture into special version
+    //   3. Player somehow creates new special sculpture
+    //      GameComponent handles adding new sculpture to list; art make special.
+    //  Weird implementation notes: master list can be used to keep track of items
+    //    in game b/c it was easier than copying it.  But saving and loading the game
+    //    makes a copy, so that's separate.
+    //    Feel free to make an actual copy, if you want to rewrite GameCompononent's logic.
+    //    If you add new logic to SpecialSculptures, in GameComponent, always check against
+    //    master list.
+    //    To be fair, I wrote this v quickly while grieving, so it's understandable
+    //    if there are questionable choices.
     public class SpecialSculpture : IExposable {
         public SpecialSculpture() {
         }
         public string id;
+        public int maxNumberCopies = 1;
         public GraphicData graphicData;
         public string titleKey;
         public string descKey;
         public string authorKey; // Just putting author ok
-        public Thing currentInstance = null; // item in game
+        public List<Thing> currentInstances = null; // item in game
         public Graphic graphic; // setup in game
         public void ExposeData() {
+            if (Scribe.mode == LoadSaveMode.Saving) {
+                // make sure we should bother saving:
+                bool shouldSave = false;
+                foreach (var t in currentInstances) {
+                    if (!t.Destroyed) {
+                        shouldSave = true;
+                        break;
+                    }
+                }
+                if (!shouldSave) return;
+            }
             Scribe_Values.Look(ref id, "id", null, true);
-            //Scribe_Values.Look(ref graphicData, "graphicData");
             Scribe_Values.Look(ref titleKey, "title");
             Scribe_Values.Look(ref descKey, "desc");
             Scribe_Values.Look(ref authorKey, "author");
-            Scribe_References.Look(ref currentInstance, "item");
+            Scribe_Collections.Look(ref currentInstances, "item", LookMode.Reference);
+            // At end of loading, go through update description and graphics of art objects
             if (Scribe.mode == LoadSaveMode.PostLoadInit) {
                 //Log.Message("PRF Loading Sculpture " + titleKey);
-                //var all = Current.Game.GetComponent<ProjectRimFactory.PRFGameComponent>().specialScupltures;
                 var all = Common.ProjectRimFactory_ModComponent.availableSpecialSculptures;
                 if (all != null) {
-//                    Log.Message("looking through game component...
                     var s = all.FirstOrDefault(x => x.id == this.id);
                     if (s!=null) {
+                        // Update graphic from master XML
                         this.graphicData = s.graphicData;
                         this.graphic = s.graphic;
+                        this.maxNumberCopies = s.maxNumberCopies;
                     }
                 }
-                MakeItemSpecial(currentInstance);
+                foreach (Thing t in this.currentInstances)
+                    MakeItemSpecial(t);
             }
         }
+        // called on system load; prepares graphics (main thread only)
         public void Init() {
             if (graphicData != null)
                 this.graphic = graphicData.Graphic;
         }
+        // Handle magic of making art item into this special sculpture
+        // Note: Description is handled by harmony patch checking against
+        //       GameComponent list
         public void MakeItemSpecial(Thing art) {
             var artComp = art.TryGetComp<CompArt>();
             if (artComp == null) { Log.Error("PRF could not make special sculpture from " + art); return; }
@@ -54,6 +89,15 @@ namespace ProjectRimFactory {
             AccessTools.Field(typeof(CompArt), "authorNameInt").SetValue(artComp, authorKey.Translate());
             if (this.graphicData != null)
                 AccessTools.Field(typeof(Thing), "graphicInt").SetValue(artComp.parent, graphicData.Graphic);
+        }
+        // Pre-load game init:
+        static public void PreStartGame() {
+            var all = Common.ProjectRimFactory_ModComponent.availableSpecialSculptures;
+            if (all != null) {
+                foreach (var s in all) {
+                    s.currentInstances = null;
+                }
+            }
         }
         // Load from XML file Settings/SpecialSculpture.xml; called from ModComponent
         static public List<SpecialSculpture> LoadAvailableSpecialSculptures(ModContentPack content) {
