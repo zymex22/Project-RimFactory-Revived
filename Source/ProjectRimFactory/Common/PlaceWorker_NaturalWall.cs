0fa34c879fba6d51b44c6de4f135ec9a0c4607de
Sn1p3rr3c0n <sn1p3rr3c0n@gmail.com>
2020-12-12 14:11:02 +0100

    #290 added initial version of the placeworker

---
+++ Source/ProjectRimFactory/Common/PlaceWorker_NaturalWall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace ProjectRimFactory.Common
+{
+
+    public class PlaceWorker_NaturalWall : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+        {
+
+            //TODO Consider Building Size and not just the center
+            List<Thing> thingList = loc.GetThingList(map);
+
+            if (thingList
+                .Where(t => t.def.IsNonResourceNaturalRock || t.def.IsSmoothed)
+                .Any())
+            {
+                return AcceptanceReport.WasAccepted;
+            }
+            else
+            {
+                return new AcceptanceReport("PRF.AutoMachineTool.MustInWall".Translate());
+            }
+        }
+    }
+
+}
