20cba1a9deb7b8c23eed55e6af733f3bff601204
lilwhitemouse <lilmouse@littlewhitemouse.net>
2020-12-14 18:41:08 -0500

    Check to ensure `thing` is around to draw row for #300

--- Source/ProjectRimFactory/Storage/UI/ITab_Items.cs
+++ Source/ProjectRimFactory/Storage/UI/ITab_Items.cs
@@ -107,6 +107,7 @@ namespace ProjectRimFactory.Storage.UI
         // Credits to LWM Deep Storage :)
         private void DrawThingRow(ref float y, float width, Thing thing)
         {
+            if (thing == null || thing.Destroyed || !thing.Spawned) return; // not here, whatever happened...
             width -= 24f;
             // row to hold the item in the GUI
             Widgets.InfoCardButton(width, y, thing);
