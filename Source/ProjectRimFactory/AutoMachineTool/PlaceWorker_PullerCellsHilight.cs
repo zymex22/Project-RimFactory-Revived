d67649d6fa06d7bbd9e003bb00665dffe24a4326
Sn1p3rr3c0n <sn1p3rr3c0n@gmail.com>
2020-12-19 10:56:54 +0100

    Added back Hilight Placeworker to Pullers (exept angeld) Added Hilight Placeworker to SAL

--- Source/ProjectRimFactory/AutoMachineTool/PlaceWorker_PullerCellsHilight.cs
+++ Source/ProjectRimFactory/AutoMachineTool/PlaceWorker_PullerCellsHilight.cs
@@ -18,6 +18,46 @@ namespace ProjectRimFactory.AutoMachineTool
 
             //Input  --> CommonColors.GetCellPatternColor(CommonColors.CellPattern.InputZone)
             //Output --> CommonColors.GetCellPatternColor(CommonColors.CellPattern.OutputZone)
+            //Log.Message("def.defName " + def.defName);
+            IntVec3 inputCell = center;
+            IntVec3 outputCell = center;
+
+
+            switch (rot.AsInt)
+            {
+                case 0:
+                    //North
+                    inputCell.z--;
+                    outputCell.z++;
+                    break;
+                case 1:
+                    //East
+                    outputCell.x++;
+                    inputCell.x--;
+                    break;
+                case 2:
+                    //South
+                    inputCell.z++;
+                    outputCell.z--;
+                    break;
+                case 3:
+                    //West
+                    outputCell.x--;
+                    inputCell.x++;
+                    break;
+                default:
+                    //Default North
+                    inputCell.z--;
+                    outputCell.z++;
+                    break;
+            }
+
+            //Not shure how i should sopport the angeled one
+
+
+
+            GenDraw.DrawFieldEdges(new List<IntVec3> { inputCell },Common.CommonColors.inputCell);
+            GenDraw.DrawFieldEdges(new List<IntVec3> { outputCell }, Common.CommonColors.outputCell);
 
 
 
