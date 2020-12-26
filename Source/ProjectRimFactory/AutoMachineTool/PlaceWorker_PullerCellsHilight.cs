3916f106b5751882a2c258ee4dde8af6dab44351
Sn1p3rr3c0n <sn1p3rr3c0n@gmail.com>
2020-12-26 21:08:17 +0100

    #319 fixed issue with bp

--- Source/ProjectRimFactory/AutoMachineTool/PlaceWorker_PullerCellsHilight.cs
+++ Source/ProjectRimFactory/AutoMachineTool/PlaceWorker_PullerCellsHilight.cs
@@ -22,11 +22,18 @@ namespace ProjectRimFactory.AutoMachineTool
             //Not shure how i should sopport the angeled one
             bool isRight = false;
 
-            if (thing != null) {
+            if (thing != null && !thing.def.IsBlueprint) {
                 isRight = (thing as Building_ItemPuller).Getright;
             }
-            outputCell = def.GetModExtension<ModExtension_Puller>().GetOutputCell(center, rot, isRight);
-
+            if (def.IsBlueprint)
+            {
+                outputCell = def.entityDefToBuild.GetModExtension<ModExtension_Puller>().GetOutputCell(center, rot, isRight);
+            }
+            else
+            {
+                outputCell = def.GetModExtension<ModExtension_Puller>().GetOutputCell(center, rot, isRight);
+            }
+            
             GenDraw.DrawFieldEdges(new List<IntVec3> { inputCell },Common.CommonColors.inputCell);
             GenDraw.DrawFieldEdges(new List<IntVec3> { outputCell }, Common.CommonColors.outputCell);
         }
