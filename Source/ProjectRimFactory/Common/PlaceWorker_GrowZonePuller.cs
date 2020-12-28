0f5ec7b9c80bb9b1b108471783eb7ce6d889285b
Sn1p3rr3c0n <sn1p3rr3c0n@gmail.com>
2020-12-28 11:45:59 +0100

    #328 added Placeworker to check if the input cell is inside of a growzone

---
+++ Source/ProjectRimFactory/Common/PlaceWorker_GrowZonePuller.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ProjectRimFactory.Common
+{
+    class PlaceWorker_GrowZonePuller : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+        {
+            IntVec3 inputCell;
+            inputCell = loc + rot.Opposite.FacingCell;
+
+            if (inputCell.GetZone(map) is Zone_Growing)
+            {
+                return AcceptanceReport.WasAccepted;
+            }
+            else
+            {
+                return new AcceptanceReport("PRF_PlaceWorker_GrowZonePuller".Translate());
+            }
+        }
+
+    }
+}
