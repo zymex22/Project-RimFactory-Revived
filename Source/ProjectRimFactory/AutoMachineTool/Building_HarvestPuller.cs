2f5c7a293fb1597465d4d62ea897d016eea3c8c5
Sn1p3rr3c0n <sn1p3rr3c0n@gmail.com>
2021-01-05 19:02:52 +0100

    fixed Harvest Pullers from ignoring takeSingleItems Setting

--- Source/ProjectRimFactory/AutoMachineTool/Building_HarvestPuller.cs
+++ Source/ProjectRimFactory/AutoMachineTool/Building_HarvestPuller.cs
@@ -20,12 +20,19 @@ namespace ProjectRimFactory.AutoMachineTool
         {
             Zone_Growing z= (this.Position + this.Rotation.Opposite.FacingCell)
                 .GetZone(this.Map) as Zone_Growing;
+            Thing target;
             if ( z == null ) return null;
-            return z.AllContainedThings
+            target = z.AllContainedThings
                     .Where(t => t.def.category == ThingCategory.Item)
                     .Where(t => !t.IsForbidden(Faction.OfPlayer) || this.takeForbiddenItems)
                     .Where(t => this.settings.AllowedToAccept(t))
                     .Where(t => !this.IsLimit(t)).FirstOrDefault<Thing>();
+
+            if (target == null) return target;
+            if (this.takeSingleItems) return (target.SplitOff(1));
+            // SplitOff ensures any item-removal effects happen:
+            return (target.SplitOff(target.stackCount));
+
         }
 
     }
