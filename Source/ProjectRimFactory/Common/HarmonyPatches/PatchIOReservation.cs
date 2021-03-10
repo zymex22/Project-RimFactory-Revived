1dc0408ac8c696bc132797739cf84c71bbddfdf3
Sn1p3rr3c0n <sn1p3rr3c0n@gmail.com>
2021-03-10 16:57:44 +0100

    #389 initial attempt by disabeling reservations for the IO Port

---
+++ Source/ProjectRimFactory/Common/HarmonyPatches/PatchIOReservation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using ProjectRimFactory.Storage;
+using UnityEngine;
+using HarmonyLib;
+
+
+namespace ProjectRimFactory.Common.HarmonyPatches
+{
+
+    [HarmonyPatch(typeof(Verse.AI.ReservationManager), "Reserve")]
+    class Patch_Reservation_Reservation_IO
+    {
+        static bool Prefix(LocalTargetInfo target ,ref bool __result , Map ___map)
+        {
+            if (target.HasThing == false && (Building_StorageUnitIOBase)target.Cell.GetThingList(___map).Where(t => t is Building_StorageUnitIOBase).FirstOrDefault() != null)
+            {
+                __result = true;
+                return false;
+            }
+
+            return true;
+
+
+        }
+    }
+}
