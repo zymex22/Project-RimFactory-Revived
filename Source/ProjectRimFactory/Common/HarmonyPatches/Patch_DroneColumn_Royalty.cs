016a5bfe7b98f56dc363b8172b164728ea197dc7
Sn1p3rr3c0n <sn1p3rr3c0n@gmail.com>
2021-03-13 19:28:05 +0100

    #412 patched Roomcounters to include Drone Columns

---
+++ Source/ProjectRimFactory/Common/HarmonyPatches/Patch_DroneColumn_Royalty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using HarmonyLib;
+using Verse;
+
+
+namespace ProjectRimFactory.Common.HarmonyPatches
+{
+    [HarmonyPatch(typeof(RoomRequirement_ThingCount), "Count")]
+    class Patch_DroneColumn_Royalty
+    {
+        static void Postfix(Room r, ref int __result , RoomRequirement_ThingCount __instance )
+        {
+            if (__result < __instance.count && __instance.thingDef == PRFDefOf.Column)
+            {
+                __result += r.ThingCount(PRFDefOf.PRF_MiniDroneColumn);
+            }
+        }
+
+    }
+}
