2e661520e8dec812bb8129b4d609038dd7c0098f
Sn1p3rr3c0n <sn1p3rr3c0n@gmail.com>
2021-04-04 09:44:47 +0200

    #411 changed it to be 1x1

--- Source/ProjectRimFactory/Storage/Building_ItemSlide.cs
+++ Source/ProjectRimFactory/Storage/Building_ItemSlide.cs
@@ -59,7 +59,7 @@ namespace ProjectRimFactory.Storage
             throw new NotImplementedException();
         }
 
-        private IntVec3 outputCell => this.Position + this.Rotation.FacingCell + this.Rotation.FacingCell;
+        private IntVec3 outputCell => this.Position + this.Rotation.FacingCell;
 
         private void trySlideItem()
         {
@@ -94,14 +94,10 @@ namespace ProjectRimFactory.Storage
         public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol, Thing thing = null)
         {
 
-            IntVec3 outputCell = center + rot.FacingCell + rot.FacingCell;
+            IntVec3 outputCell = center + rot.FacingCell;
 
 
             GenDraw.DrawFieldEdges(new List<IntVec3> { outputCell }, Common.CommonColors.outputCell);
-
-
-
-
 
         }
     }
