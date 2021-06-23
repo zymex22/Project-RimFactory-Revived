b2be010bfba10d10c7c48c30e8f5497dad50a4ba
Sn1p3rr3c0n <sn1p3rr3c0n@gmail.com>
2021-06-23 22:18:35 +0200

    #454 Updated the defName Replacement

--- Source/ProjectRimFactory/Common/BackCompatibility/PRF_BackCompatibilityConverter.cs
+++ Source/ProjectRimFactory/Common/BackCompatibility/PRF_BackCompatibilityConverter.cs
@@ -24,13 +24,57 @@ namespace ProjectRimFactory.Common.BackCompatibility
 			{
 				switch (defName)
 				{
-                    //Dummy Values UPDATE before Merge!
-					case "PRF_BeltsI":
-						return "PRF_Research_BeltsI";
-                    case "PRF_BeltsII":
-                        return "PRF_Research_BeltsII";
-                    case "PRF_BeltsIII":
-                        return "PRF_Research_BeltsIII";
+                    //PRF Materials Rename
+					case "PRF_StainlessSteelAlloying":
+						return "PRF_Research_StainlessSteelAlloying";
+
+                    case "PRF_Carbon":
+                        return "PRF_Research_Carbon";
+                    case "PRF_CarbonNanotubes":
+                        return "PRF_Research_CarbonNanotubes";
+                    case "PRF_CarbonFiber":
+                        return "PRF_Research_CarbonFiber";
+                    case "PRF_CarbonKevlar":
+                        return "PRF_Research_CarbonKevlar";
+                    case "PRF_CarbonPlating":
+                        return "PRF_Research_CarbonPlating";
+                    case "PRF_SyntheticDiamond":
+                        return "PRF_Research_SyntheticDiamond";
+
+                    case "PRF_AdvancedFlooring":
+                        return "PRF_Research_AdvancedFlooring";
+
+                    case "PRF_CokeProduction":
+                        return "PRF_Research_CokeProduction";
+                    case "PRF_Brick":
+                        return "PRF_Research_Brick";
+                    case "PRF_BasicGlass":
+                        return "PRF_Research_BasicGlass";
+
+                    case "PRF_ReinforcedGlass":
+                        return "PRF_Research_ReinforcedGlass";
+                    case "PRF_Cement":
+                        return "PRF_Research_Cement";
+                    case "PRF_BasicConcrete":
+                        return "PRF_Research_BasicConcrete";
+
+                    case "PRF_BulkConcrete":
+                        return "PRF_Research_BulkConcrete";
+
+                    case "PRF_AdvancedConcrete":
+                        return "PRF_Research_AdvancedConcrete";
+
+                    case "PRF_BasicPlastics":
+                        return "PRF_Research_BasicPlastics";
+                    case "PRF_AdvPlastics":
+                        return "PRF_Research_AdvPlastics";
+                    case "PRF_BasicPlasticTextiles":
+                        return "PRF_Research_BasicPlasticTextiles";
+                    case "PRF_AdvPlasticTextiles":
+                        return "PRF_Research_AdvPlasticTextiles";
+                    case "PRF_GlitterPlasticTextiles":
+                        return "PRF_Research_GlitterPlasticTextiles";
+
 
                 }
 			}
