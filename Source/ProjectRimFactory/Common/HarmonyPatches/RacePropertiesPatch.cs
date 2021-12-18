4c905cd2db6348161edf06bb8d7815f6c98a0617
Sn1p3rr3c0n <Sn1p3rr3c0n@gmail.com>
2021-12-18 12:41:59 +0100

    #545 changed patch to always run

--- Source/ProjectRimFactory/Common/HarmonyPatches/RacePropertiesPatch.cs
+++ Source/ProjectRimFactory/Common/HarmonyPatches/RacePropertiesPatch.cs
@@ -1,49 +1,18 @@
 ﻿using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Verse;
-using RimWorld;
-using ProjectRimFactory.Drones;
 
 namespace ProjectRimFactory.Common.HarmonyPatches
 {
 
-    // A patch to the problem of forbidding what drones have mined.
-    // When mineable yields, if pawn is Drone, Drone will be Colonist.
-    [HarmonyPatch(typeof(Mineable), "TrySpawnYield")]
-    static class Patch_Mineable_TrySpawnYield
-    {
-        static void Prefix(Mineable __instance, Map map, float yieldChance, bool moteOnWaste, Pawn pawn)
-        {
-            if (pawn is Pawn_Drone)
-            {
-                Patch_Pawn_IsColonist.overrideIsColonist = true;
-            }
-        }
-
-        static void Postfix(Mineable __instance, Map map, float yieldChance, bool moteOnWaste, Pawn pawn)
-        {
-            if (pawn is Pawn_Drone)
-            {
-                Patch_Pawn_IsColonist.overrideIsColonist = false;
-            }
-        }
-    }
-
-    // A patch to the problem of forbidding what drones have mined.
-    // When mineable yields, if pawn is Drone, Drone will be Colonist.
     [HarmonyPatch(typeof(Pawn), "get_IsColonist")]
     static class Patch_Pawn_IsColonist
     {
         static void Postfix(Pawn __instance, ref bool __result)
         {
-            if (overrideIsColonist && __instance is Pawn_Drone && !__result && __instance.Faction != null && __instance.Faction.IsPlayer)
+            if (!__result && __instance.kindDef == PRFDefOf.PRFDroneKind)
             {
                 __result = true;
             }
         }
-        public static bool overrideIsColonist = false;
     }
 }
