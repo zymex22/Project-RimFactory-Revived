9b9c7422da0cccb63293f1b13933967ba0d04ced
Sn1p3rr3c0n <Sn1p3rr3c0n@gmail.com>
2023-02-20 19:53:34 +0100

    #680 fixed issue where drone Skills where ignored

--- Source/ProjectRimFactory/Common/HarmonyPatches/Patch_GenRecipe_MakeRecipeProducts.cs
+++ Source/ProjectRimFactory/Common/HarmonyPatches/Patch_GenRecipe_MakeRecipeProducts.cs
@@ -1,4 +1,5 @@
 using HarmonyLib;
+using ProjectRimFactory.Drones;
 using ProjectRimFactory.SAL3.Things.Assemblers;
 using RimWorld;
 using System;
@@ -151,6 +152,60 @@ namespace ProjectRimFactory.Common.HarmonyPatches
             }
             return true;
         }
+
+        //Prevent Biotech(Mech Changes) from interfering with Drone Skills
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+
+            bool foundReplaceMarker = false;
+            bool repacedCheck = false;
+            int cnt = 0;
+            foreach (var instruction in instructions)
+            {
+                cnt++;
+                //Search for IL_0000: ldarg.0 -> Loading the Pawn
+                if (instruction.opcode == OpCodes.Ldarg_0 && !repacedCheck)
+                {
+                    repacedCheck = true;
+                    foundReplaceMarker = true;
+                    cnt = 0;
+                }
+                //remove IL_0001: callvirt instance class Verse.RaceProperties Verse.Pawn::get_RaceProps()
+                if (instruction.opcode == OpCodes.Callvirt && foundReplaceMarker && cnt == 1)
+                {
+                    continue;
+                }
+                if (instruction.opcode == OpCodes.Callvirt && foundReplaceMarker && cnt == 2)
+                {
+                    //Replace IL_0006: callvirt instance bool Verse.RaceProperties::get_IsMechanoid()
+                    //with a call to Patch_GenRecipe_GenerateQualityCreatedByPawn:IsMechanoid(Pawn pawn)
+                    instruction.operand = AccessTools.Method(
+                        typeof(Patch_GenRecipe_GenerateQualityCreatedByPawn),
+                        nameof(Patch_GenRecipe_GenerateQualityCreatedByPawn.IsMechanoid), new[] { typeof(Pawn)});
+                    foundReplaceMarker = false;
+                }
+
+
+                yield return instruction;
+            }
+
+        }
+
+        /// <summary>
+        /// if a Pawn is a Mechanoid then it's skills will be ignored by QualityUtility:GenerateQualityCreatedByPawn
+        /// This prevents Drones from being detected as Mechanoid
+        /// </summary>
+        /// <param name="pawn"></param>
+        /// <returns></returns>
+        public static bool IsMechanoid(Pawn pawn)
+        {
+            if (pawn is Pawn_Drone)
+            {
+                return false;
+            }
+            return pawn.RaceProps.IsMechanoid;
+        }
+
     }
 
     interface ISetQualityDirectly
