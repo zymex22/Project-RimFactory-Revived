b2f2fe06d60c6e4ff8dfc178d3eaac6c288b592b
Sn1p3rr3c0n <Sn1p3rr3c0n@gmail.com>
2023-02-24 16:47:50 +0100

    #647 added support for Quality Builder

---
+++ Source/ProjectRimFactory/Common/HarmonyPatches/Patch_QualityBuilder_getPawnConstructionSkill.cs
@@ -0,0 +1,25 @@
+﻿using ProjectRimFactory.Drones;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ProjectRimFactory.Common.HarmonyPatches
+{
+    internal class Patch_QualityBuilder_getPawnConstructionSkill
+    {
+        static public bool Prefix(out int __result, Pawn pawn)
+        {
+            __result = 0;
+            if (pawn is Pawn_Drone)
+            {
+                __result = pawn.skills.GetSkill(SkillDefOf.Construction).Level;
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Source/ProjectRimFactory/Common/HarmonyPatches/Patch_QualityBuilder_pawnCanConstruct.cs
@@ -0,0 +1,22 @@
+﻿using ProjectRimFactory.Drones;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ProjectRimFactory.Common.HarmonyPatches
+{
+    class Patch_QualityBuilder_pawnCanConstruct
+    {
+        public static void Postfix(Pawn pawn, ref bool __result)
+        {
+            if (!__result && pawn is Pawn_Drone && (pawn.workSettings?.WorkIsActive(WorkTypeDefOf.Construction) ?? false))
+            {
+                __result = true;
+            }
+        }
+    }
+}
