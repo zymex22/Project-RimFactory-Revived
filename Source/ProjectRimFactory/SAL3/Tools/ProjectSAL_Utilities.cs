91e5a8f424adb167a10dc64e1f0691c0177a613d
Sn1p3rr3c0n <Sn1p3rr3c0n@gmail.com>
2023-02-28 09:10:14 +0100

    #684 fixed Dominant Ingrediant Calc Ensured SAL and Assemblers use the same Calc code

--- Source/ProjectRimFactory/SAL3/Tools/ProjectSAL_Utilities.cs
+++ Source/ProjectRimFactory/SAL3/Tools/ProjectSAL_Utilities.cs
@@ -2,31 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using Verse;
+using Verse.AI;
 
 namespace ProjectRimFactory.SAL3.Tools
 {
     static class ProjectSAL_Utilities
     {
-        public static Thing CalculateDominantIngredient(RecipeDef currentRecipe, IEnumerable<Thing> thingRecord)
+        /// <summary>
+        /// Modified Version of Verse.AI.Toils_Recipe:CalculateDominantIngredient to work without a Job
+        /// </summary>
+        /// <param name="RecipeDef"></param>
+        /// <param name="ingredients"></param>
+        /// <returns></returns>
+        public static Thing CalculateDominantIngredient(RecipeDef RecipeDef, List<Thing> ingredients)
         {
-            var stuffs = thingRecord.Where(t => t.def.IsStuff);
-            if (!thingRecord.Any())
+            if (!ingredients.NullOrEmpty())
             {
-                if (currentRecipe.ingredients.Count > 0) Log.Warning("S.A.L.: Had no thingRecord of items being accepted, but crafting recipe has ingredients. Did you reload a save?");
-                return ThingMaker.MakeThing(ThingDefOf.Steel);
-            }
-            if (stuffs.Any())
-            {
-                if (currentRecipe.productHasIngredientStuff)
+                if (RecipeDef.productHasIngredientStuff)
                 {
-                    return stuffs.OrderByDescending(x => x.stackCount).First();
+                    return ingredients[0];
                 }
-                if (currentRecipe.products.Any(x => x.thingDef.MadeFromStuff))
+                if (RecipeDef.products.Any((ThingDefCountClass x) => x.thingDef.MadeFromStuff))
                 {
-                    return stuffs.RandomElementByWeight(x => x.stackCount);
+                    return ingredients.Where((Thing x) => x.def.IsStuff).RandomElementByWeight((Thing x) => x.stackCount);
                 }
+                return ingredients.RandomElementByWeight((Thing x) => x.stackCount);
             }
-            return ThingMaker.MakeThing(ThingDefOf.Steel);
+            return null;
         }
     }
 }
