08a69adaf3919d7aed158f6392591c5f4d1f53a2
Sn1p3rr3c0n <Sn1p3rr3c0n@gmail.com>
2023-08-23 14:01:59 +0200

    #732 added Jobdef and driver

---
+++ Source/ProjectRimFactory/AutoMachineTool/JobDriver_StaticJob.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse.AI;
+
+namespace ProjectRimFactory.AutoMachineTool
+{
+    public class JobDriver_StaticJob : JobDriver
+    {
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
+        {
+            return true;
+        }
+
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            return null;
+        }
+    }
+}
