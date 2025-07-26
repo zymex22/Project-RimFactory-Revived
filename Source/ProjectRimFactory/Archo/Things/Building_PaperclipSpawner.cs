a31dd9310f1a565eabe0309bb8011e80306df415
Sn1p3rr3c0n <Sn1p3rr3c0n@gmail.com>
2025-07-26 09:12:38 +0200

    Cleanup Insanity stuff

--- Source/ProjectRimFactory/Archo/Things/Building_PaperclipSpawner.cs
+++ Source/ProjectRimFactory/Archo/Things/Building_PaperclipSpawner.cs
@@ -5,11 +5,12 @@ using Verse;
 
 namespace ProjectRimFactory.Archo.Things
 {
+    // ReSharper disable once UnusedType.Global
     public class Building_PaperclipSpawner : Building
     {
-        int workDone;
-        CompPowerTrader powerComp;
-        CompOutputAdjustable outputComp;
+        private int workDone;
+        private CompPowerTrader powerComp;
+        private CompOutputAdjustable outputComp;
         public override void PostMake()
         {
             base.PostMake();
@@ -29,24 +30,23 @@ namespace ProjectRimFactory.Archo.Things
             if (this.IsHashIntervalTick(10) && powerComp.PowerOn)
             {
                 workDone++;
-                if (workDone >= 250)
-                {
-                    Thing result = ThingMaker.MakeThing(PRFDefOf.Paperclip); // Spawns 10000 paperclips per batch
-                    result.stackCount = 10000;
-                    GenPlace.TryPlaceThing(result, outputComp.CurrentCell, Map, ThingPlaceMode.Near);
-                    workDone = 0;
-                }
+                if (workDone < 250) return;
+                var result = ThingMaker.MakeThing(PRFDefOf.Paperclip); // Spawns 10000 paperclips per batch
+                result.stackCount = 10000;
+                GenPlace.TryPlaceThing(result, outputComp.CurrentCell, Map, ThingPlaceMode.Near);
+                workDone = 0;
             }
         }
         public override string GetInspectString()
         {
-            StringBuilder builder = new StringBuilder();
-            string str = base.GetInspectString();
+            var builder = new StringBuilder();
+            var str = base.GetInspectString();
             if (!string.IsNullOrEmpty(str))
             {
                 builder.AppendLine(str);
             }
-            builder.Append("NextSpawnedItemIn".Translate(GenLabel.ThingLabel(PRFDefOf.Paperclip, null, 10000)) + ": " + ((250 - workDone) * 10).ToStringTicksToPeriod());
+            builder.Append("NextSpawnedItemIn".Translate(
+                GenLabel.ThingLabel(PRFDefOf.Paperclip, null, 10000)) + ": " + ((250 - workDone) * 10).ToStringTicksToPeriod());
             return builder.ToString().TrimEndNewlines();
         }
         public override void ExposeData()
